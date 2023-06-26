95b80972cdbc2dbfe72fafd895cd5e6156533dcd
rraf1 <137785667+rraf1@users.noreply.github.com>
2023-06-26 13:31:42 +0100

    Create view.sql

---
+++ scripts/view.sql
@@ -0,0 +1,14 @@
+--I can't understand what's meant by required fields since it was not mentioned previously so I'll just create a view that makes sense:
+CREATE VIEW v_item_loc_soh (
+    SELECT i.item,
+           i.item_desc,
+           i.dept,
+           l.loc,
+           l.loc_desc,
+           s.unit_cost,
+           s.stock_on_hand
+      FROM item i,
+           loc l,
+           item_loc_soh s
+     WHERE s.item = i.item
+       AND s.loc = l.loc );
