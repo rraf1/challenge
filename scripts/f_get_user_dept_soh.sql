a397d84ea90cb27f29e38f76cb2f46335207daf9
rraf1 <137785667+rraf1@users.noreply.github.com>
2023-06-26 13:58:02 +0100

    Update f_get_user_dept_soh.sql

--- scripts/f_get_user_dept_soh.sql
+++ scripts/f_get_user_dept_soh.sql
@@ -1,12 +1,14 @@
---I'm not sure what's meant by "filter mechanism" so I'll just create a function that returns the data available to a given user/store
-CREATE OR REPLACE FUNCTION f_get_user_dept_soh (p_user user_dept.user%TYPE
-                                                p_loc  loc.loc%TYPE) 
-  RETURN SYS_REFCURSOR AS
+--I'm not sure what's meant by "filter mechanism" so I'll just create a procedure that returns the data available to a given user/store
+CREATE OR REPLACE PROCEDURE p_get_user_dept_soh (p_user_id     user_dept.user_id%TYPE,
+                                                 p_loc         loc.loc%TYPE,
+                                                 p_data OUT    sys_refcursor)
+IS
 BEGIN
-  RETURN SELECT *
-           FROM item_loc_soh s,
-                user_dept    u
-          WHERE u.user = p_user
-            AND s.loc  = p_loc
-            AND s.dept = u.dept;
-END f_get_user_dept_soh;
+  OPEN p_data FOR
+    SELECT s.*
+      FROM item_loc_soh s,
+           user_dept    u
+     WHERE u.user_id = p_user_id
+       AND s.loc     = p_loc
+       AND s.dept    = u.dept;
+END p_get_user_dept_soh;
